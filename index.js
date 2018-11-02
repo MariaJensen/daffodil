2a9e1755feee3a3f11d8ab3a7f7768b34bb269d4
Maria <30218672+MariaJensen@users.noreply.github.com>
2018-11-02 08:59:43 +0100

    typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 const server = http.createServer( (req, res) => {
 	res.end('test');
