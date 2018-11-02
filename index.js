065ca99c284f3897827bb0ee59ad527af923912d
Maria <30218672+MariaJensen@users.noreply.github.com>
2018-11-02 10:02:28 +0100

    serving static files

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 'use strict';
 
 const http = require('http');
+const path = require('path');
 const express = require('express');
 
 const port = process.env.PORT || 3001;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use('/', express.static(path.join(__dirname, 'client/build')));
+
+app.get('/test', (req, res) => {
 	res.send('testing 1,2');
 });
+
+app.get('/', (req, res) => {
+
+})
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
